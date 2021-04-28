3e4f8e2fc7e34dd5194408c822e6b8603cfcae6e
Harouna <dev.harouna@gmail.com>
2021-04-28 04:26:07 +0000

    Configuration setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+	name='image-pdf-tools',
+	long_description=long_description,
+	long_description_content_type='text/markdown'
+)
