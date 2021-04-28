77577184ad9b83f63a428b854a240079d2828a8e
Harouna <dev.harouna@gmail.com>
2021-04-28 04:37:28 +0000

    Fix name package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-	name='image-pdf-tools',
+	name='django-forms-fieldset',
 	long_description=long_description,
 	long_description_content_type='text/markdown'
 )
