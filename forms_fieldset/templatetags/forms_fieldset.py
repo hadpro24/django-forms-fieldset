c1741ddb1dd4c37c454ffd055115481c17c3c15e
Harouna <dev.harouna@gmail.com>
2021-05-08 05:28:03 +0000

    [DM] Add i18n to text "Ajout supplimentaire" for english langue #3

--- forms_fieldset/templatetags/forms_fieldset.py
+++ forms_fieldset/templatetags/forms_fieldset.py
@@ -1,6 +1,7 @@
 from django import template
 from django.contrib.admin.helpers import AdminForm
 from django.template.loader import render_to_string
+from django.conf import settings
 
 class FieldsetsNotSupported(Exception):
 	""" form field sets type error, it must be a list """
@@ -20,9 +21,21 @@ def fieldset(form, fieldsets, color='#79AEC8'):
 
 @register.simple_tag
 def inline_fieldset(inline_formset, color='#79AEC8', label='Inline Form'):
+	LANGEAGE_DATA = {
+		'en-en': 'Add another',
+		'en': 'Add another',
+		'EN-EN': 'Add another',
+		'EN': 'Add another',
+		'fr-fr': 'Ajout supplémentaire',
+		'fr': 'Ajout supplémentaire',
+		'FR-FR': 'Ajout supplémentaire',
+		'Fr': 'Ajout supplémentaire',
+		'de': 'Ajout supplémentaire',
+	}
 	context = {
 		'inline_formset': inline_formset,
 		'color': color,
 		'label': label,
+		'title_add': LANGEAGE_DATA.get(settings.LANGUAGE_CODE, 'Add another') 
 	}
 	return render_to_string('forms_fieldset/tabular.html', context)
